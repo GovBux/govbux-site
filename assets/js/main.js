f8dd912179af7a2354dfa38689bd40f832f8da9f
GovBux <admin@govbux.com>
2024-12-17 21:01:28 -0500

    Initial Upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll("[data-include]");
+    elements.forEach((el) => {
+        const file = el.getAttribute("data-include");
+        if (file) {
+            fetch(file)
+                .then(response => response.text())
+                .then(data => el.innerHTML = data)
+                .catch(error => console.error(`Error loading ${file}: ${error}`));
+        }
+    });
+});
